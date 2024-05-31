a600eb4475174587609074428e49f88ef5d48cf4
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2024-05-31 19:02:54 +0530

    sending messages and creating embeds

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -18,12 +19,37 @@ func checkNilErr(err error) {
 	}
 }
 
+const prefix string = "!"
+
 func msgHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	if m.Content == "ping" {
+	args := strings.Split(m.Content, " ")
+	if args[0] != prefix {
+		return
+	}
+
+	if args[1] == "hello" && args[2] == "there" {
+
+		// Author:
+		author := &discordgo.MessageEmbedAuthor{
+			Name: "notion",
+			URL:  "https://www.notion.so/PClub-Core-db3b8b06fdd64562ae60eee03e95e029",
+		}
+
+		// creating and embed:
+		embed := &discordgo.MessageEmbed{
+			Title:  "General Kenobi!",
+			Author: author,
+		}
+
+		// s.ChannelMessageSend(m.ChannelID, "General Kenobi!")
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	}
+
+	if args[1] == "ping" {
 		s.ChannelMessageSend(m.ChannelID, "pong")
 	}
 }
