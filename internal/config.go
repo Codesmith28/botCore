2903a2bf45aad9080ef2bcee8d2d8fdb5ee02a03
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-06-23 22:33:23 +0530

    refactor config and env handling

--- internal/config.go
+++ internal/config.go
@@ -9,62 +9,63 @@ import (
 )
 
 var (
-	Token        string
-	DatabaseId   string
-	NotionSecret string
-	ChannelId    string
-	MongoURI     string
+	Token              string
+	DatabaseId         string
+	NotionSecret       string
+	GeneralChannelId   string
+	AnalyticsChannelId string
+	MongoURI           string
 
 	MongoClient     *mongo.Client
 	MongoCollection *mongo.Collection
 )
 
 func init() {
-	// Try to load the environment variables from Render first
-	Token = os.Getenv("DISCORD_TOKEN")
-	NotionSecret = os.Getenv("NOTION_SECRET")
-	DatabaseId = os.Getenv("NOTION_DATABASE_ID")
-	ChannelId = os.Getenv("DISCORD_CHANNEL_ID")
-	MongoURI = os.Getenv("MONGO_URI")
+	envVars := map[string]*string{
+		"DISCORD_TOKEN":                &Token,
+		"NOTION_SECRET":                &NotionSecret,
+		"NOTION_DATABASE_ID":           &DatabaseId,
+		"DISCORD_CHANNEL_ID_GENERAL":   &GeneralChannelId,
+		"DISCORD_CHANNEL_ID_ANALYTICS": &AnalyticsChannelId,
+		"MONGO_URI":                    &MongoURI,
+	}
+
+	loadEnvVars(envVars)
+	checkEnvVars(envVars)
+}
+
+func loadEnvVars(envVars map[string]*string) {
+	// Try to load the environment variables from the system first
+	for key, ptr := range envVars {
+		*ptr = os.Getenv(key)
+	}
 
 	// If any environment variable is not set, load from .env file
-	if Token == "" || NotionSecret == "" || DatabaseId == "" || ChannelId == "" || MongoURI == "" {
+	missingEnv := false
+	for _, ptr := range envVars {
+		if *ptr == "" {
+			missingEnv = true
+			break
+		}
+	}
+
+	if missingEnv {
 		err := godotenv.Load()
 		if err != nil {
 			log.Printf("Error loading .env file: %v", err)
 		}
 
-		if Token == "" {
-			Token = os.Getenv("DISCORD_TOKEN")
-		}
-		if NotionSecret == "" {
-			NotionSecret = os.Getenv("NOTION_SECRET")
-		}
-		if DatabaseId == "" {
-			DatabaseId = os.Getenv("NOTION_DATABASE_ID")
-		}
-		if ChannelId == "" {
-			ChannelId = os.Getenv("DISCORD_CHANNEL_ID")
-		}
-		if MongoURI == "" {
-			MongoURI = os.Getenv("MONGO_URI")
+		for key, ptr := range envVars {
+			if *ptr == "" {
+				*ptr = os.Getenv(key)
+			}
 		}
 	}
-
-	checkEnvVar()
 }
 
-func checkEnvVar() {
-	envVars := map[string]string{
-		"DISCORD_TOKEN":      Token,
-		"NOTION_DATABASE_ID": DatabaseId,
-		"NOTION_SECRET":      NotionSecret,
-		"DISCORD_CHANNEL_ID": ChannelId,
-		"MONGO_URI":          MongoURI,
-	}
-
-	for name, value := range envVars {
-		if value == "" {
+func checkEnvVars(envVars map[string]*string) {
+	for name, ptr := range envVars {
+		if *ptr == "" {
 			log.Fatalf("%s not found in environment variables or .env file", name)
 		}
 	}
