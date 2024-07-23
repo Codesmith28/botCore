51833a76bc726b8c814088de7ab0082ac60fb522
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-07-23 17:54:52 +0530

    refactor notion message and codecleaning

--- discordHandler/discordHandler.go
+++ discordHandler/discordHandler.go
@@ -1,16 +1,11 @@
 package discordHandler
 
 import (
-	"fmt"
 	"log"
-	"strings"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 
-	"github.com/Codesmith28/botCore/database"
 	"github.com/Codesmith28/botCore/internal"
-	"github.com/Codesmith28/botCore/notionHandler"
 )
 
 var MemberMap = internal.MemberMap
@@ -19,52 +14,6 @@ func checkNilErr(err error) {
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-func TaskMessageHandler(sess *discordgo.Session, ready *discordgo.Ready) {
-	channelID := internal.GeneralChannelId
-
-	lastSent, err := database.ReadLastSent()
-	checkNilErr(err)
-
-	// // debug message list:
-	// MsgList := MessageMaker()
-
-	now := time.Now()
-	if now.Sub(lastSent) < 24*time.Hour {
-		log.Println("Message already sent today, skipping...")
-		return
-	}
-
-	// connect to notion now:
-	notionHandler.NotionConnect()
-	MsgList := MessageMaker()
-
-	for _, message := range MsgList {
-		// Prepare the mentions
-		var mentions []string
-		for _, assignee := range message.Assignees {
-			if discordID, exists := MemberMap[assignee]; exists {
-				mentions = append(mentions, fmt.Sprintf("<@%s>", discordID))
-			}
-		}
-
-		// Construct the message with mentions
-		content := fmt.Sprintf("## %s\n %s\nDays Left: %d\nAssignees: %s",
-			message.Title,
-			message.Message,
-			message.DaysLeft,
-			strings.Join(mentions, " "),
-		)
-
-		// Send the message to the channel
-		_, err := sess.ChannelMessageSend(channelID, content)
-		checkNilErr(err)
-	}
-
-	// update the lastSent
-	err = database.WriteLastSent(now)
-	checkNilErr(err)
 }
 
 func BotHandler(sess *discordgo.Session) {
--- discordHandler/notionMessenger.go
+++ discordHandler/notionMessenger.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
+	"github.com/bwmarrin/discordgo"
+
+	"github.com/Codesmith28/botCore/database"
 	"github.com/Codesmith28/botCore/internal"
 	"github.com/Codesmith28/botCore/notionHandler"
 )
 
 type Message = internal.Message
 
-// Convert assignee names to Discord mentions
 func getAssigneeMentions(assignees []string) string {
 	var mentions []string
 	for _, assignee := range assignees {
@@ -64,4 +67,47 @@ func MessageMaker() []Message {
 	}
 
 	return MessageList
+}
+
+func TaskMessageHandler(sess *discordgo.Session, ready *discordgo.Ready) {
+	channelID := internal.GeneralChannelId
+
+	lastSent, err := database.ReadLastSent()
+	checkNilErr(err)
+
+	now := time.Now()
+	if now.Sub(lastSent) < 24*time.Hour {
+		log.Println("Message already sent today, skipping...")
+		return
+	}
+
+	// connect to notion now:
+	notionHandler.NotionConnect()
+	MsgList := MessageMaker()
+
+	for _, message := range MsgList {
+		// Prepare the mentions
+		var mentions []string
+		for _, assignee := range message.Assignees {
+			if discordID, exists := MemberMap[assignee]; exists {
+				mentions = append(mentions, fmt.Sprintf("<@%s>", discordID))
+			}
+		}
+
+		// Construct the message with mentions
+		content := fmt.Sprintf("## %s\n %s\nDays Left: %d\nAssignees: %s",
+			message.Title,
+			message.Message,
+			message.DaysLeft,
+			strings.Join(mentions, " "),
+		)
+
+		// Send the message to the channel
+		_, err := sess.ChannelMessageSend(channelID, content)
+		checkNilErr(err)
+	}
+
+	// update the lastSent
+	err = database.WriteLastSent(now)
+	checkNilErr(err)
 }
