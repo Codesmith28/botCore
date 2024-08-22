d1048088c68a29c534e8d653021c0520835a4a9b
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-08-22 07:16:37 +0530

    add dev mode

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,15 @@
 import dotenv from "dotenv";
 import { MongoClient as MongoDbClient, Collection } from "mongodb";
+import path from "path";
 
-dotenv.config();
+const NODE_ENV = process.env.NODE_ENV || "production";
+
+// Load the appropriate .env file
+if (NODE_ENV === "development") {
+    dotenv.config({ path: path.resolve(process.cwd(), ".dev.env") });
+} else {
+    dotenv.config();
+}
 
 const requiredEnvVars = [
     "GUILD_ID",
@@ -19,22 +27,21 @@ const requiredEnvVars = [
 ] as const;
 
 function loadAndCheckEnvVars() {
-    const env: { [key: string]: any } = {};
-
+    const env: { [key: string]: string } = {};
     for (const key of requiredEnvVars) {
         const value = process.env[key];
         if (!value) {
             throw new Error(
-                `${key} not found in environment variables or .env file`
+                `${key} not found in environment variables or {NODE_ENV === "development" ? ".dev.env" : ".env"} file`,
             );
         }
         env[key] = value;
     }
-
     return env;
 }
 
 export const env = loadAndCheckEnvVars();
-
 export let MongoClient: typeof MongoDbClient;
 export let MongoCollection: Collection;
+
+console.log(`Running in ${NODE_ENV} mode`);
