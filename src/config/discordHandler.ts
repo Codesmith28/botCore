45b3bea6287b163c584c2c4b32e46f6a4bac20eb
Codesmith28 <sarthaksiddhpura@gmail.com>
2024-08-28 21:38:14 +0530

    decrease tick fq of analytics handler

--- src/config/discordHandler.ts
+++ src/config/discordHandler.ts
@@ -84,6 +84,6 @@ export function botHandler(client: Client) {
         });
 
         setInterval(() => taskMessageHandler(client), 30 * 60 * 1000);
-        setInterval(() => analyticsMessageHandler(client), 30 * 60 * 1000);
+        setInterval(() => analyticsMessageHandler(client), 24 * 60 * 60 * 1000);
     });
 }
